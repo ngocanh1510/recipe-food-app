2d435fabd7a98f36c02b8266d6c6e2bfce03d51b
lan <lengoclan2501@gmail.com>
2024-11-30 08:42:00 +0700

    update account table

--- backend/src/models/Account.js
+++ backend/src/models/Account.js
@@ -2,8 +2,18 @@ import mongoose from "mongoose"
 import UserModel from "./User.js"
 
 const AccountSchema = new mongoose.Schema({
-    username: {type: mongoose.Schema.Types.ObjectId, required: true, ref: UserModel},
+    username: {
+        type: String,
+        required: true, 
+        unique: true,
+        trim: true
+    },
     password: {type: String, require: true},
+    member: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "member", 
+        required: true 
+    },
 });
 
 const AccountModel = mongoose.model("accounts", AccountSchema);
