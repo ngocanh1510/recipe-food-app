afb804e6254f6d60afa12e4fccddc62c64dfb2d9
Anh <22520073@gm.uit.edu.vn>
2024-12-02 22:08:56 +0700

    up

--- backend/src/controllers/recipe-controller.js
+++ backend/src/controllers/recipe-controller.js
@@ -124,3 +124,59 @@ export const getAllRecipes = async (req, res, next) => {
   
     return res.status(201).json({ message: "Recipe added successfully", recipe })
     };
+
+    
+    export const editRecipe = async (req, res) => {
+      const { id: recipeId, use } = req.params;   // Lấy showtimeId từ tham số URL
+      const { movie_id, screening_room_id, date, language } = req.body;  // Lấy dữ liệu từ body
+  
+      // Kiểm tra xem showtimeId và các trường cần cập nhật có hợp lệ không
+      if (!mongoose.Types.ObjectId.isValid(showtimeId)) {
+          return res.status(400).json({ message: "Invalid Showtime ID" });
+      }
+  
+     
+      // Tạo đối tượng lưu trữ các trường cần cập nhật
+      const updateFields = {};
+  
+      if (movie_id && mongoose.Types.ObjectId.isValid(movie_id)) {
+          updateFields.movie_id = new mongoose.Types.ObjectId(movie_id);  // Cập nhật movie_id
+      }
+  
+      if (screening_room_id && mongoose.Types.ObjectId.isValid(screening_room_id)) {
+          updateFields.screening_room_id = new mongoose.Types.ObjectId(screening_room_id);  // Cập nhật screening_room_id
+      }
+  
+      if (date && new Date(date).toString() !== "Invalid Date") {
+          updateFields.date = new Date(date);  // Cập nhật ngày chiếu
+      }
+  
+      if (language && language.trim() !== "") {
+          updateFields.language = language.trim();  // Cập nhật ngôn ngữ
+      }
+  
+      // Nếu không có trường hợp nào hợp lệ để cập nhật
+      if (Object.keys(updateFields).length === 0) {
+          return res.status(422).json({ message: "No valid fields to update" });
+      }
+  
+      let updatedShowtime;
+      try {
+          
+          // Cập nhật showtime với showtimeId và các trường hợp cần thiết
+          updatedShowtime = await Showtime.findByIdAndUpdate(
+              showtimeId,
+              updateFields,
+              { new: true }  // Trả về dữ liệu đã cập nhật
+          );
+          
+          // Kiểm tra xem showtime có tồn tại hay không
+          if (!updatedShowtime) {
+              return res.status(404).json({ message: "Showtime not found" });
+          }
+  
+          return res.status(200).json({ message: "Showtime updated successfully", showtime: updatedShowtime });
+      } catch (error) {
+          return res.status(500).json({ message: "Failed to update showtime", error: error.message });
+      }
+  };
